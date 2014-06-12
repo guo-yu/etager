2323bf650edf965dc53a29ba8707ad821faaa37d
Guo Yu <i@guoyu.me>
2014-06-12 17:45:04 +0800

    format codes

--- demo/server.js
+++ demo/server.js
@@ -1,57 +1,57 @@
-var http = require('http'),
-    express = require('express'),
-    path = require('path');
+var http = require('http');
+var express = require('express');
+var path = require('path');
 
 var etager = require('../');
 
-var Server = function() {
+function Server() {
 
-    var app = express();
+  var app = express();
 
-    // all environments
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser({
-        keepExtensions: true,
-        uploadDir: path.join(__dirname, '/uploads')
-    }));
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('demo'));
-    app.use(etager.listen(function(uuid, request, firstaccess){
-        if (firstaccess) {
-            console.log('hi newbie [' + uuid + ']');
-        } else {
-            console.log('welcome back [' + uuid + ']');
-        }
-    }));
-    app.use(app.router);
-
-    // development only
-    if ('development' == app.get('env')) {
-        app.use(express.errorHandler());
+  // all environments
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser({
+    keepExtensions: true,
+    uploadDir: path.join(__dirname, '/uploads')
+  }));
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('demo'));
+  app.use(etager.listen(function(uuid, request, firstaccess) {
+    if (firstaccess) {
+      console.log('hi newbie [' + uuid + ']');
+    } else {
+      console.log('welcome back [' + uuid + ']');
     }
+  }));
+  app.use(app.router);
 
-    app.get('/demo/*',function(req,res,next){
-        res.send(
-            [
-            '<h1>',
-            'hello' + req.url,
-            '</h1>',
-            res.locals.tracker
-            ].join('\n')
-            )
-    })
+  // development only
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
 
-    this.app = app;
+  app.get('/demo/*', function(req, res, next) {
+    res.send(
+      [
+        '<h1>',
+        'hello' + req.url,
+        '</h1>',
+        res.locals.tracker
+      ].join('\n')
+    )
+  })
+
+  this.app = app;
 }
 
 Server.prototype.run = function(port) {
-    var self = this;
-    if (port && !isNaN(parseInt(port))) {
-        self.app.set('port', parseInt(port));
-    } else {
-        self.app.set('port', 3333);
-    }
-    http.createServer(self.app).listen(self.app.get('port'));
+  var self = this;
+  if (port && !isNaN(parseInt(port))) {
+    self.app.set('port', parseInt(port));
+  } else {
+    self.app.set('port', 3333);
+  }
+  http.createServer(self.app).listen(self.app.get('port'));
 }
 
 new Server().run();
--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 var uuid = require('node-uuid');
 
 exports.tracker = function() {
-    this.image = 'tracker.jpg';
-    this.url = '/' + this.image;
-    this.html = "<img src='" + this.image + "' style='display:none !important;' />";
-    return this;
+  this.image = 'tracker.jpg';
+  this.url = '/' + this.image;
+  this.html = "<img src='" + this.image + "' style='display:none !important;' />";
+  return this;
 }
 
-exports.listen = function(cb) {
-    var tracker = exports.tracker();
-    return function(req, res, next) {
-        var keepTag = function(tag) {
-                res.set('ETag', tag);
-                res.send('');
-            };
-        // 如果访问tracker
-        if (req.url.indexOf(tracker.url) > -1) {
-            var tag = req.headers['if-none-match'];
-            if (tag) {
-                cb(tag, req, false);
-                keepTag(tag);
-            } else {
-                var uid = uuid.v1();
-                cb(uid, req, true);
-                keepTag(uid);
-            }
-        } else {
-            // 如果访问其他url
-            if (!res.locals.tracker) {
-                res.locals.tracker = tracker.html;
-            }
-            next()
-        }
-    };
+exports.listen = function(callback) {
+  var tracker = exports.tracker();
+  return function(req, res, next) {
+    // 如果访问tracker
+    if (req.url.indexOf(tracker.url) > -1) {
+      var tag = req.headers['if-none-match'];
+      if (tag) {
+        callback(tag, req, false);
+        keepTag(tag);
+      } else {
+        var uid = uuid.v1();
+        callback(uid, req, true);
+        keepTag(uid);
+      }
+    } else {
+      // 如果访问其他 uri
+      if (!res.locals.tracker) {
+        res.locals.tracker = tracker.html;
+      }
+      return next();
+    }
+
+    function keepTag(tag) {
+      res.set('ETag', tag);
+      res.send('');
+    }
+  }
 }
